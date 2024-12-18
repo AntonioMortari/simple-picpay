86105a0494c7adf244bc04d392e4f4ca8c83e954
Antonio Mortari <antoniomortari09@gmail.com>
2024-12-18 20:00:52 -0300

    chore: add port configuration and verbose logging to server startup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
+
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  logger.verbose(`Server running on port ${port} 🚀`);
 }
 bootstrap();
