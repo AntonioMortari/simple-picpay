482263f9fd96a415352e166a9b830efd28e852f1
Antonio Mortari <antoniomortari09@gmail.com>
2024-12-18 20:31:03 -0300

    feat(User): add user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/infra/repository/user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(dto: CreateUserDto) {
+    await this.validateUserUnique(dto);
+
+    return await this.userRepository.create(dto);
+  }
+
+  private async validateUserUnique(dto: CreateUserDto) {
+    const { email, document } = dto;
+
+    const findEmail = await this.userRepository.getByEmail(email);
+
+    if (findEmail) {
+      throw new BadRequestException(`User with email ${email} exists`);
+    }
+
+    const findDocument = await this.userRepository.getByDocument(document);
+
+    if (findDocument) {
+      throw new BadRequestException(`User with document ${document} exists`);
+    }
+  }
+}
