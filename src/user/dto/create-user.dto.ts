b6dc97c792fd1a1f6b1a31c6325670765607f2d0
Antonio Mortari <antoniomortari09@gmail.com>
2024-12-18 20:08:18 -0300

    feat(User): add CreateUserDto

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import { RoleTypes } from '@prisma/client';
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  document: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  role?: RoleTypes;
+
+  @IsNumber()
+  @IsOptional()
+  balance: number;
+}
